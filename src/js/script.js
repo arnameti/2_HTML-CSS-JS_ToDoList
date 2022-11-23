cc845f796c8b1e9ee2bec13bc2780767c48cdddf
Arnor Ameti <arnameti@gmail.com>
2022-11-23 16:48:30 +0100

    feat: creating needed selectors

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+const todoConctainerEL = document.querySelector('[data-container]');
+const formEl = document.querySelector('[data-form]');
+const inputEl = formEl.querySelector('[data-input]');
+const todoContainer = document.querySelector('[data-todo]');
